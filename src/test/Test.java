58e04be4b8c435cd10d7ead837c2358f97998d07
huangqf <huangqf@LAPTOP-92VQ28MH.domain.name>
2018-10-13 16:11:08 +0800

    111

--- src/test/Test.java
+++ src/test/Test.java
@@ -10,7 +10,7 @@ public class Test {
 		//1、excel后缀为xlsm，为启用宏的工作簿
 		//2、避免弹出警告框，操作步骤：文件-选项-信任中心-信任中心设置-隐私选项中把“保存时从文件属性中删除个人信息”前的勾号去掉
 		tool.OpenExcel(System.getProperty("user.dir").replace("\\", "/") + "/src/test/JacobTest.xlsm",false,false);
-		//调用Excel宏
+		//调用Excel宏 
 		tool.callMacro("createPicture");
 		tool.callMacro("createPicture2");
 		//关闭并保存，释放对象
